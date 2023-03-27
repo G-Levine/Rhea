4eaa4a87d932128456478c5565fe931f4049c101
G-Levine <gabrael@gmail.com>
2023-03-26 17:26:43 -0700

    Fix API byte decoding

--- api_input_manager.py
+++ api_input_manager.py
@@ -29,16 +29,17 @@ class UserInputManager():
             self.user_input['x'] = 0
             self.user_input['yaw'] = 0
         try:
-            message = str(socket.recv(zmq.NOBLOCK))
-            if message == "stand":
+            message = socket.recv(zmq.NOBLOCK)
+            message = message.decode('utf8')
+            if "stand" in message:
                 self.user_input['stand_up_toggle'] = True
-            elif message == "sit":
+            elif "sit" in message:
                 self.user_input['stand_up_toggle'] = False
             elif "move" in message:
                 command_substrings = message.split("_")
                 self.user_input['x'] = command_substrings[1]
                 self.user_input['yaw'] = command_substrings[2]
-                self.command_end_time = time.time() + float(command_substrings[3][:-1])
+                self.command_end_time = time.time() + float(command_substrings[3])
             response = b"Got command"
             socket.send(response)
         except zmq.Again:
