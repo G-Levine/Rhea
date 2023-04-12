381116aa13a4034035c214545115babe704aab66
selenasun1618 <selenas@stanford.edu>
2023-04-12 00:28:27 -0700

    Turn around when collision is detected

--- api_input_manager.py
+++ api_input_manager.py
@@ -60,9 +60,10 @@ class UserInputManager():
             message = collision_monitor_socket.recv(zmq.NOBLOCK)
             message = message.decode('utf8')
             if "collision" in message:
+                # If collided, stop moving and turn around
                 self.user_input['x'] = 0
-                self.user_input['yaw'] = 0
-                self.command_end_time = time.time()
+                self.user_input['yaw'] = 1.57
+                self.command_end_time = time.time() + 2.0
             response = b"Got collision notification"
             collision_monitor_socket.send(response)
         except zmq.Again:
